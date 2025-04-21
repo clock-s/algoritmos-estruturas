575dd76f98f01515789addc03c2e3dccca2bd037
Davi <85353824+clock-s@users.noreply.github.com>
2025-04-21 02:03:23 -0300

    Polonesa
    Implementação da polonesa com prioridade de operações

--- algoritimos/polonesa.cpp
+++ algoritimos/polonesa.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-
-string polonesa_para_expressao(string polonesa, bool rev = 1);
-string expressao_para_polonesa(string expressao, bool rev = 1);
+void att_expressao(stack<string> &num, stack<string> &oper, const bool rev);
+string polonesa_para_expressao(string polonesa, bool rev = REVERSE);
+string expressao_para_polonesa(string expressao, bool rev = REVERSE);
+string expressao_para_polonesa_complexa(string expressao, bool rev = REVERSE);
 int operador(char caracter);
 
 
@@ -22,6 +23,69 @@ int operador(char caracter){
     case '^': return 3;
     default: return 0;
     }
+
+}
+
+
+void att_expressao(stack<string> &num, stack<string> &oper, const bool rev){
+    string B = num.top(); num.pop(); 
+    string A = num.top(); num.pop(); 
+    string operador = oper.top(); oper.pop(); 
+
+    string expressao;
+    if(rev) expressao =  A + B + operador;
+    else if(!rev) expressao = operador + A + B;
+
+    
+    num.push(expressao);
+}
+
+
+string expressao_para_polonesa_complexa(string expressao, bool rev){
+    stack<string> num;
+    stack<string> oper;
+
+    for(int i = 0 ; i < expressao.size() ; i++){
+        string caracter(1, expressao[i]);
+
+        cout << "i: " << i << " " << caracter;
+        if(!oper.empty()) cout << " " << oper.top();
+        cout << endl;
+        if(caracter == " ") continue; //Não é necessário analisar caso em que é branco
+        else if(caracter == "(") oper.push(caracter);
+
+        else if(!num.empty() && !oper.empty() && (operador(caracter[0]) > 0) && (operador(caracter[0]) < operador(oper.top()[0]))){
+            while(!oper.empty() && oper.top() != "(") att_expressao(num, oper, rev);
+            if(!oper.empty() && oper.top() == "(") oper.pop();
+            oper.push(caracter);
+        }
+            
+            
+
+        else if(operador(caracter[0]) > 0) oper.push(caracter); //Coloca os operadores na lista de operador e pula para o pŕoximo loop
+
+        else if(caracter == ")" && !num.empty() && !oper.empty()){ //Realiza a operação
+            while(!oper.empty() && oper.top() != "("){
+                att_expressao(num, oper, rev);
+                
+            }
+            if(!oper.empty() && oper.top() == "(") oper.pop();
+        }
+
+        else if(caracter != ")") num.push(caracter); //O que sobrar ou é letra ou número, então fica sendo considerado número e vai para pilha de numero
+
+        if(!num.empty()) cout << num.top() << endl;
+    }
+
+    
+    //Checagem final se o usuário não colocar o parentese mais externo
+    while(!oper.empty()){
+        att_expressao(num, oper, rev);
+        if(!oper.empty() && oper.top() == "(") oper.pop();
+    }
+
+
+    return num.top(); //No fim, a pilha só terá um elemento sendo ele a polonesa final
 
 }
 
@@ -40,33 +104,17 @@ string expressao_para_polonesa(string expressao, bool rev){
         else if(operador(caracter[0]) > 0) oper.push(caracter); //Coloca os operadores na lista de operador e pula para o pŕoximo loop
 
         else if(caracter == ")" && !num.empty() && !oper.empty()){ //Realiza a operação
-            string B = num.top(); num.pop();
-            string A = num.top(); num.pop();
-            string operador = oper.top(); oper.pop();
-
-            string expressao;
-            if(rev) expressao =  A + B + operador;
-            else if(!rev) expressao = operador + A + B;
-
-            num.push(expressao);
+            att_expressao(num, oper, rev);
         }
 
-        else num.push(caracter); //O que sobrar ou é letra ou número, então fica sendo considerado número e vai para pilha de numero
+        else if(caracter != ")") num.push(caracter); //O que sobrar ou é letra ou número, então fica sendo considerado número e vai para pilha de numero
 
     }
 
 
     //Checagem final se o usuário não colocar o parentese mais externo
     while(!oper.empty()){
-        string B = num.top(); num.pop();
-        string A = num.top(); num.pop();
-        string operador = oper.top(); oper.pop();
-
-        string expressao;
-        if(rev) expressao =  A + B + operador;
-        else if(!rev) expressao = operador + A + B;
-
-        num.push(expressao);
+        att_expressao(num, oper, rev);
     }
 
     
@@ -114,13 +162,13 @@ string polonesa_para_expressao(string polonesa, bool rev){
 
 int main(int argc, char const *argv[]){
 
-    string expresao = "(((R + S) * T) - U)";
+    string expresao = "A+B*C^D + D^E*F+G";
 
-    string polonesa = expressao_para_polonesa(expresao, NORMAL);
+    string polonesa = expressao_para_polonesa_complexa(expresao);
 
-    cout << polonesa << endl;
+    cout <<"Polonesa: " << polonesa << endl;
 
-    string nova_expressao = polonesa_para_expressao(polonesa, NORMAL);
+    string nova_expressao = polonesa_para_expressao(polonesa);
 
     cout << nova_expressao << endl;
 
